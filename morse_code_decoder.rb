5001654af757212e5ab5cd6f82c0a0b956462b25
Marc raphael <marcraphael979@gmail.com>
2023-04-12 13:35:18 +0100

    Add decode a a character and word methods

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -26,3 +26,25 @@
   '-.--' => 'Y',
   '--..' => 'Z'
 }
+
+#first we'll need 3 methods
+#decode a character
+def decode_a_character(str)
+	@dictionary[str]
+end
+
+decode_a_character('.-')
+#decode a word
+
+def decode_a_word (str)
+  @word_array = []
+
+  #we divide our word into isolated character
+  simple_character = str.split
+  simple_character.each do |i|
+    @word_array.push(decode_a_character(i))
+  end
+end
+
+decode_a_word('.-')
+#decode the whole message
